86a4dc37bb8601c51461f19d063f1394d43f0054
mayarasantana19 <mayarasantana1630@gmail.com>
2025-08-20 11:58:52 -0300

    alterações 20/08

--- Projeto/js/compare.js
+++ Projeto/js/compare.js
@@ -6,7 +6,17 @@ class Car {
    
 
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+      this.nome = nome;
+      this.preco = preco;
+      this.alturaCacamba = alturaCacamba;
+      this.alturaVeiculo = alturaVeiculo;
+      this.alturaSolo = alturaSolo;
+      this.capacidadeCarga = capacidadeCarga;
+      this.motor = motor;
+      this.potencia = potencia;
+      this.volumeCacamba = volumeCacamba;
+      this.roda = roda;
+      this.image = image;
     }
 } 
 
@@ -23,14 +33,28 @@ function SetCarToCompare(el, carClass) {
    
     if(carClass instanceof Car){       
         if(el.checked){
-                
+            if(carArr.length >=2){
+                el.checked = false;
+                alert("Você só pode comparar 2 veículos por vez!");
+                return
+            }
+                if(GetCarArrPosition(carArr, carClass) === -1)
+                carArr.push(carClass);
             
         } else {
-          
+          const index = GetCarArrPosition(carArr, carClass);
+          if(index !== -1){
+            carArr.splice(index, 1);
+          }
         } 
+
     } else {
         throw "You need set a Car Class";
     }
+}
+
+function botaoComparar(){
+    const compareBtn = document.querySelector("button[onclick='ShowCompare()']")
 }
 
 function ShowCompare() {
@@ -38,6 +62,8 @@ function ShowCompare() {
         alert("Precisa marcar 2 carros para apresentar a comparação");
         return;
     }
+
+
 
     UpdateCompareTable();
     document.getElementById("compare").style.display = "block";
