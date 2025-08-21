6b3aae69cf26b67d5927d611cb14ff207c251881
mayarasantana19 <mayarasantana1630@gmail.com>
2025-08-21 09:27:56 -0300

    Resolvendo conflitos no carousel

--- Projeto/js/carousel.js
+++ Projeto/js/carousel.js
@@ -11,13 +11,8 @@ class Carousel {
 
     constructor(image, title, uri) {
         this.image = image;
-<<<<<<< HEAD
-        this.title = title
-        this.uri = uri
-=======
         this.title = title;
         this.uri = uri;
->>>>>>> 7a4a5c04fa1d2b2d7297e79943a0ff098c1f6c2a
     }
 
     static Start(arr) {
@@ -29,13 +24,10 @@ class Carousel {
             Carousel._interval = setInterval(function () { Carousel.Next(); }, 2000);
 
         } else {
-<<<<<<< HEAD
             //"Method Start need a Array Variable.";//
             throw "O método start precisa receber um array válido."
-=======
             // throw "Method Start need a Array Variable.";
             throw "O método Start precisa receber um array válido."
->>>>>>> 7a4a5c04fa1d2b2d7297e79943a0ff098c1f6c2a
         }
     }
 
@@ -43,13 +35,8 @@ class Carousel {
         const carrouselElement = document.getElementById("carousel");
         const titleElement = document.getElementById("carousel-title");
 
-<<<<<<< HEAD
-        if (!carrouselElement || !titleElement ){
-            console.error("elementos do carrossel não encontrados!");
-=======
         if (!carrouselElement || !titleElement){
             console.error("Elementos do carrossel não encontrados!");
->>>>>>> 7a4a5c04fa1d2b2d7297e79943a0ff098c1f6c2a
             return;
         }
 
@@ -61,14 +48,7 @@ class Carousel {
         carrouselElement.style.transition = "background-image 0.5s ease-in-out";
 
         titleElement.innerHTML = `<a href= "${item.uri}"> ${item.title}</a>`;
-<<<<<<< HEAD
-
-        Carousel._sequence = (Carousel._sequence +1) % Carousel._size;
-
-    
-=======
         
         Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
->>>>>>> 7a4a5c04fa1d2b2d7297e79943a0ff098c1f6c2a
     }
 };
